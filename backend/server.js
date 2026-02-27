64a3a4acfa4cf94083d192b65c6d9e71bc167114
Nikeshala22 <sadarenunikeshala@gmail.com>
2026-02-27 09:15:38 +0530

    Add some routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 
 import authRoutes from "./routes/authRoutes.js"; 
+import reportRoutes from "./routes/wellReportRoutes.js";
+import wellsRoutes from "./routes/wellsRoutes.js";
 import waterQualityRoutes from "./routes/waterQualityRoutes.js";
 import maintenanceRequestRoutes from "./routes/maintenanceRequestRoutes.js"; 
 
@@ -18,15 +20,18 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
+app.use("/api/wells", wellsRoutes);
+app.use("/api/reports", reportRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/water-quality", waterQualityRoutes);
+
 
 // app.get('/', (req, res)=> res.send("Server is running"))
 
 
 app.use("/api/maintenance", maintenanceRequestRoutes);
 
-
+app.use("/uploads", express.static("uploads"));
 
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 5000;
